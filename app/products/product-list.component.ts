18b3ceb93464a1aa1717ad538f4091f18cd0a5ab
Jonathan Musso <jonathanmusso@gmail.com>
2016-06-09 20:41:36 -0400

    Created Navigation. Implemented routing.

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from 'angular2/core';
+import { ROUTER_DIRECTIVES } from 'angular2/router';
 
 import { IProduct } from './product'
 import { ProductFilterPipe } from './product-filter.pipe'
@@ -6,11 +7,10 @@ import { StarComponent } from '../shared/star.component'
 import { ProductService } from './product.service'
 
 @Component({
-    selector: 'pm-products',
     templateUrl: 'app/products/product-list.component.html',
     styleUrls: ['app/products/product-list.component.css'],
     pipes: [ProductFilterPipe],
-    directives: [StarComponent]    
+    directives: [StarComponent, ROUTER_DIRECTIVES]    
 })
 
 export class ProductListComponent implements OnInit {
